62842ca2432928d11720c4cc01f1b831cbf5922f
Brendan Clark <bpclark@loyola.edu>
2023-11-12 18:04:09 -0500

    100% coverage for entities

---
+++ src/test/java/model/entity/FriendshipTest.java
@@ -0,0 +1,46 @@
+package model.entity;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import pokersite.model.entity.Friendship;
+import java.sql.Timestamp;
+import java.time.Instant;
+
+public class FriendshipTest {
+    @Test public void testEmptyFriendship() {
+        Friendship fs = new Friendship();
+        assertAll(
+                ()-> assertNull(fs.getID()),
+                ()-> assertNull(fs.getId_user1()),
+                ()-> assertNull(fs.getId_user2()),
+                ()-> assertNull(fs.getDt_accepted())
+        );
+    }
+
+    @Test public void testConstructor() {
+        Timestamp ts = Timestamp.from(Instant.now());
+        Friendship fs = new Friendship(10, 1, 2, ts);
+        assertAll(
+                ()-> assertEquals(10, fs.getID()),
+                ()-> assertEquals(1, fs.getId_user1()),
+                ()-> assertEquals(2, fs.getId_user2()),
+                ()-> assertEquals(ts, fs.getDt_accepted())
+        );
+    }
+
+    @Test public void testSetters() {
+        Timestamp ts = Timestamp.from(Instant.now());
+        Friendship fs = new Friendship();
+        fs.setID(10);
+        fs.setId_user1(1);
+        fs.setId_user2(2);
+        fs.setDt_accepted(ts);
+        assertAll(
+                ()-> assertEquals(10, fs.getID()),
+                ()-> assertEquals(1, fs.getId_user1()),
+                ()-> assertEquals(2, fs.getId_user2()),
+                ()-> assertEquals(ts, fs.getDt_accepted())
+        );
+    }
+}
--- src/test/java/model/entity/UserTest.java
+++ src/test/java/model/entity/UserTest.java
@@ -1,9 +1,9 @@
 package model.entity;
 
 import org.junit.jupiter.api.Test;
-import pokersite.model.entity.User;
-
 import static org.junit.jupiter.api.Assertions.*;
+
+import pokersite.model.entity.User;
 
 public class UserTest {
 
@@ -22,13 +22,34 @@ public class UserTest {
     @Test public void testConstructor() {
         User u = new User(10, "Bill", "billthebest", "bill@gmail.com", "bill", "guy", "8564738561");
         assertAll(
-                ()-> assertEquals(u.getID(), 10),
-                ()-> assertEquals(u.getUsername(), "Bill"),
-                ()-> assertEquals(u.getPassword(), "billthebest"),
-                ()-> assertEquals(u.getEmail(), "bill@gmail.com"),
-                ()-> assertEquals(u.getFirst_name(), "bill"),
-                ()-> assertEquals(u.getLast_name(), "guy"),
-                ()-> assertEquals(u.getPhone_number(), "8564738561")
+                ()-> assertEquals(10, u.getID()),
+                ()-> assertEquals("Bill", u.getUsername()),
+                ()-> assertEquals("billthebest", u.getPassword()),
+                ()-> assertEquals("bill@gmail.com", u.getEmail()),
+                ()-> assertEquals("bill", u.getFirst_name()),
+                ()-> assertEquals("guy", u.getLast_name()),
+                ()-> assertEquals("8564738561", u.getPhone_number())
+        );
+    }
+
+    @Test public void testSetters() {
+        User u = new User();
+        u.setID(10);
+        u.setUsername("Bill");
+        u.setPassword("billthebest");
+        u.setEmail("bill@gmail.com");
+        u.setFirst_name("bill");
+        u.setLast_name("guy");
+        u.setPhone_number("8564738561");
+
+        assertAll(
+                ()-> assertEquals(10, u.getID()),
+                ()-> assertEquals("Bill", u.getUsername()),
+                ()-> assertEquals("billthebest", u.getPassword()),
+                ()-> assertEquals("bill@gmail.com", u.getEmail()),
+                ()-> assertEquals("bill", u.getFirst_name()),
+                ()-> assertEquals("guy", u.getLast_name()),
+                ()-> assertEquals("8564738561", u.getPhone_number())
         );
     }
 }
