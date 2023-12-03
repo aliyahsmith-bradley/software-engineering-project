c3486f004277b596fb4ca6c07a230fae23c05b78
AudreyVersteegen <audrey@versteegen.us>
2023-12-03 14:23:09 -0500

    game logic refactoring

---
+++ src/main/java/pokersite/Logic/fivedraw/ComputerPlayerLogic.java
@@ -0,0 +1,14 @@
+package pokersite.Logic.fivedraw;
+
+public class ComputerPlayerLogic extends PlayerLogic {
+    String compName = "Placeholder";
+    int compType = 1;
+    int baseBetAmount = 10;
+    boolean checked = false;
+
+    public void chooseBet(){
+
+    }
+
+
+}
